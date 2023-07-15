e667f4b50b47aa45327b6d7bcef2690b054281f2
Yassir Ismaaili <yismaail@e1r5p4.1337.ma>
2023-07-15 15:50:55 +0100

    add Makefile

--- Cpp00/ex00/megaphone.cpp
+++ Cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: yismaail <yismaail@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 08:19:08 by yismaail          #+#    #+#             */
-/*   Updated: 2023/07/15 08:19:09 by yismaail         ###   ########.fr       */
+/*   Updated: 2023/07/15 08:33:01 by yismaail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,6 @@
 
 using namespace std;
 
-void	doUpper(int ac, char **av)
-{
-	string	str;
-
-	if (ac == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	else
-	{
-		for (int i = 1; i < ac; i++)
-		{
-			str = strToUpper(av[i]);
-			cout << str;
-		}
-	}
-}
 
 string strToUpper(string str)
 {
@@ -45,16 +30,31 @@ string strToUpper(string str)
 	}
 	return (str);
 }
+void	doUpper(int ac, char **av)
+{
+	string	str;
+
+	if (ac == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			str = strToUpper(av[i]);
+			cout << str << endl;
+		}
+	}
+}
 
 int main(int ac, char **av)
 {
-	doUpper(int ac, char **av);
-	string str;
+	doUpper(ac, av);
+	// string str;
 
-	for (int i = 1; i < ac; i++)
-	{
-		str = strToUpper(av[i]);
-		cout << str;
-	}
+	// for (int i = 1; i < ac; i++)
+	// {
+	// 	str = strToUpper(av[i]);
+	// 	cout << str;
+	// }
 	return (0);
 }
