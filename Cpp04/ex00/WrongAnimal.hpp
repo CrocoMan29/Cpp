30f2f4a533f505d7a1f430833e0c75ab1cb17e3a
Yassir Ismaaili <yismaail@e2r3p4.1337.ma>
2023-10-16 01:58:07 +0100

    incha2alah

---
+++ Cpp04/ex00/WrongAnimal.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   WrongAnimal.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yismaail <yismaail@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 01:46:17 by yismaail          #+#    #+#             */
+/*   Updated: 2023/10/16 01:46:48 by yismaail         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class WrongAnimal
+{
+    protected:
+        std::string _type;
+    public:
+        WrongAnimal();
+        WrongAnimal(WrongAnimal const & src);
+        WrongAnimal &operator=(WrongAnimal const &obj);
+        virtual ~WrongAnimal();
+        std::string getType() const;
+        void setType(std::string type);
+        void makeSound() const;
+};
