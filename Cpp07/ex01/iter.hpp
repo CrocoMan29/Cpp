e6fb70d0392394f2750b87c77244694c10fc636a
Yassir Ismaaili <yismaail@e2r1p8.1337.ma>
2023-12-23 14:59:45 +0100

    check ex01

--- Cpp07/ex01/iter.hpp
+++ Cpp07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: yismaail <yassirismaaili8@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 02:59:59 by yismaail          #+#    #+#             */
-/*   Updated: 2023/12/22 03:30:20 by yismaail         ###   ########.fr       */
+/*   Updated: 2023/12/23 14:53:53 by yismaail         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ template<typename T, typename func>
 void iter(T *arr, size_t lenght, func f){
 	for (size_t i = 0; i < lenght; i++)
 	{
-		f(arr[i]++);
+		f(arr[i]);
 	}
 }
 
 template<typename T>
-void print(T element){
+void print(T &element){
 	std::cout << element << std::endl;
 }
